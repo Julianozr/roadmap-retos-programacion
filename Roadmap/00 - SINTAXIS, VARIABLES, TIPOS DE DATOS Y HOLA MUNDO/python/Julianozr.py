a7550235787c79a8f4e7a7c07b8e077b76ffa768
Julianozr <110164237+Julianozr@users.noreply.github.com>
2024-01-05 07:24:21 -0500

    Create Julianozr.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Julianozr.py
@@ -0,0 +1,12 @@
+# Este es mi primer repositorio en Github y la url del lenguaje de programación que voy a usar es https://python.org
+#Para un comentario en python es el simbolo numeral
+"""
+Para 
+un 
+comentario
+en varias lineas es triple comillas dobles
+"""
+'''
+o
+simples
+'''
